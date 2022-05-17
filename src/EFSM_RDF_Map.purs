14bd7c2e9804f302c8bc472f7125d411b12870c9
Daniel Schraudner <daniel.schraudner@fau.de>
2022-05-17 09:21:18 +0200

    Accepting tasks

--- src/EFSM_RDF_Map.purs
+++ src/EFSM_RDF_Map.purs
@@ -3,13 +3,16 @@ module EFSMRDFMap where
 import Prelude
 
 import Control.Alternative (guard)
+import Data.List (List(..), (:))
 import Data.Maybe (Maybe(..))
-import Data.Set (filter, findMax, fromFoldable, size)
+import Data.Set (filter, findMax, fromFoldable, size, union)
 import Data.Tuple (Tuple(..))
-import EFSM (EFSMConfig, Variables)
-import RDF (Graph, defaultGraph, literalType, namedNode, namedNode', object, predicate, quad, subject, termToBoolean, termToInt)
-import RDF.Prefixes (ra, rdf, xsd)
-import RobotArm (D)
+import EFSM (EFSMConfig, Input, Variables)
+import RDF (Graph, Quad, Term, defaultGraph, literalType, namedNode, namedNode', object, predicate, quad, subject, termToBoolean, termToInt, termType, value)
+import RDF.Prefixes (ldp, ra, rdf, xsd)
+import RobotArm (D, a, b, c, d)
+
+data TaskState = TaskAccepted | TaskRunning | TaskSuccessful | TaskFailed
 
 rdfForArm :: String -> EFSMConfig D -> Graph
 rdfForArm base (Tuple _ { pos, closed, item }) = fromFoldable [
@@ -21,10 +24,33 @@ rdfForArm base (Tuple _ { pos, closed, item }) = fromFoldable [
   quad (namedNode $ base <> "#robotArm") (namedNode' ra "item") (literalType (show item) (namedNode' xsd "boolean")) defaultGraph
 ]
 
-rdfForTasks :: String -> Graph
-rdfForTasks base = fromFoldable [
+rdfForTasks :: String -> List (Tuple String (Tuple Input TaskState)) -> Graph
+rdfForTasks base inputs = union (fromFoldable [
   quad (namedNode $ base <> "tasks/") (namedNode' rdf "type") (namedNode' ra "TaskContainer") defaultGraph
+]) (fromFoldable $ inputsToContainsQuads base inputs)
+  where
+    inputsToContainsQuads :: String -> List (Tuple String (Tuple Input TaskState)) -> List Quad
+    inputsToContainsQuads base' ((Tuple idx _) : is) =
+      (quad (namedNode $ base <> "tasks/") (namedNode' ldp "contains") (namedNode $ base <> "tasks/" <> idx) defaultGraph)
+      : inputsToContainsQuads base' is
+    inputsToContainsQuads _ Nil = Nil
+
+rdfForTask :: String -> Tuple String (Tuple Input TaskState) -> Graph
+rdfForTask base (Tuple idx (Tuple input taskState)) = fromFoldable [
+  quad (namedNode $ base <> "tasks/" <> idx) (namedNode' rdf "type") (task input) defaultGraph,
+  quad (namedNode $ base <> "tasks/" <> idx) (namedNode' ra "taskState") (state taskState) defaultGraph
 ]
+  where
+    task :: Input -> Term
+    task input' = if input' == a then namedNode' ra "MoveFrom1To2Task" else 
+      if input' == b then namedNode' ra "MoveFrom2To3Task" else
+      if input' == c then namedNode' ra "MoveFrom2To1Task" else
+      if input' == d then namedNode' ra "MoveFrom3To2Task" else namedNode' ra "Task"
+    state :: TaskState -> Term
+    state TaskAccepted = namedNode' ra "accepted"
+    state TaskRunning = namedNode' ra "running"
+    state TaskSuccessful = namedNode' ra "successful"
+    state TaskFailed = namedNode' ra "failed"
 
 rdfForEvents :: String -> Graph
 rdfForEvents base = fromFoldable [
@@ -50,3 +76,19 @@ armForRdf base g = if correctSize && correctType then do
     closedQuads = filter (\q -> subject q == (namedNode $ base <> "#robotArm") && predicate q == (namedNode' ra "closed")) g
     itemQuads = filter (\q -> subject q == (namedNode $ base <> "#robotArm") && predicate q == (namedNode' ra "item")) g
     posQuads = filter (\q -> subject q == (namedNode $ base <> "#robotArm") && predicate q == (namedNode' ra "pos")) g
+
+taskForRdf :: String -> Graph -> Maybe Input
+taskForRdf uri g = if correctSize then do
+    guard $ size typeQuads == 1
+    t <- object <$> findMax typeQuads
+    guard $ termType t == "NamedNode"
+    case value t of 
+      "https://solid.ti.rw.fau.de/public/ns/robotArm#MoveFrom1To2Task" -> Just a
+      "https://solid.ti.rw.fau.de/public/ns/robotArm#MoveFrom2To3Task" -> Just b
+      "https://solid.ti.rw.fau.de/public/ns/robotArm#MoveFrom2To1Task" -> Just c
+      "https://solid.ti.rw.fau.de/public/ns/robotArm#MoveFrom3To2Task" -> Just d
+      _ -> Nothing
+  else Nothing
+  where
+    correctSize = size g == 1
+    typeQuads = filter (\q -> subject q == (namedNode uri) && predicate q == (namedNode' rdf "type")) g
--- src/Main.purs
+++ src/Main.purs
@@ -4,19 +4,24 @@ import Prelude
 
 import Control.Monad.State (runState)
 import Data.Either (Either(..))
+import Data.Foldable (maximum)
+import Data.List (List(..), find, (:))
 import Data.Maybe (Maybe(..))
 import Data.Set (fromFoldable)
 import Data.Tuple (Tuple(..))
-import EFSM (EFSMConfig, updateVariables)
-import EFSMRDFMap (armForRdf, rdfForArm, rdfForEvents, rdfForTasks)
+import EFSM (EFSMConfig, Input, Output, processInput, updateVariables)
+import EFSMRDFMap (TaskState(..), armForRdf, rdfForArm, rdfForEvents, rdfForTask, rdfForTasks, taskForRdf)
+import Effect.Aff (Milliseconds(..), delay)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
-import Effect.Ref (Ref, new, read, write)
-import HTTPure (Headers, Method(..), Request, ResponseM, ServerM, badRequest, conflict, headers, noContent, notFound, ok', serve, toString)
+import Effect.Ref (Ref, modify, new, read, write)
+import HTTPure (Headers, Method(..), Request, ResponseM, ServerM, badRequest, conflict, created, headers, noContent, notFound, ok', serve, toString, (!@))
 import RDF (Graph, blankNode, defaultGraph, literalLang, namedNode, quad, serialize, triple)
 import RDFPS.NTriplesParser (parse)
-import RobotArm (D, e, s0)
+import RobotArm (D, a, b, e, s0)
 import Text.Parsing.Parser (parseErrorMessage)
+
+type ServerState = Record ( config :: EFSMConfig D, tasks :: List (Tuple String (Tuple Input TaskState)), events :: List (Tuple Int Output) )
 
 baseURI :: String
 baseURI = "http://localhost:8080/"
@@ -27,14 +32,11 @@ ntHeader = headers [
   Tuple "Accept" "application/n-triples"
 ]
 
-initialEFSMConfig :: EFSMConfig D
-initialEFSMConfig = Tuple s0 { pos: 1, closed: true, item: false }
-
-router :: Ref (EFSMConfig D) -> Request -> ResponseM
-router config { path: [], method: Get } = do
-  c <- liftEffect $ read config
-  ok' ntHeader $ serialize $ rdfForArm baseURI c
-router config { path: [], method: Put, body: body } = do
+router :: Ref ServerState -> Request -> ResponseM
+router state { path: [], method: Get } = do
+  { config } <- liftEffect $ read state
+  ok' ntHeader $ serialize $ rdfForArm baseURI config
+router state { path: [], method: Put, body: body } = do
   b <- toString body
   let gr = parse b
   case gr of
@@ -44,12 +46,44 @@ router config { path: [], method: Put, body: body } = do
       case vars of 
         Nothing -> badRequest "Variables could not be extracted from RDF"
         Just vars' -> do
-          c <- liftEffect $ read config
-          let (Tuple success c') = runState (updateVariables e vars') c
-          _ <- liftEffect $ write c' config
+          { config, tasks, events } <- liftEffect $ read state
+          let (Tuple success config') = runState (updateVariables e vars') config
+          _ <- liftEffect $ write { config: config', tasks: tasks, events: events } state
           if success then noContent else conflict ""
-router config { path: ["tasks"], method: Get } = ok' ntHeader (serialize $ rdfForTasks baseURI)
-router config { path: ["events"], method: Get } = ok' ntHeader (serialize $ rdfForEvents baseURI)
+router state { path: ["tasks"], method: Get } = do 
+  { tasks } <- liftEffect $ read state
+  ok' ntHeader (serialize $ rdfForTasks baseURI tasks)
+router state { path: ["events"], method: Get } = ok' ntHeader (serialize $ rdfForEvents baseURI)
+router state { path, method: Get }
+  | path !@ 0 == "tasks" && not (path !@ 1 == "") = do
+    { tasks } <- liftEffect $ read state
+    case find (\(Tuple idx _) -> path !@ 1 == idx) tasks of 
+      Just task -> ok' ntHeader (serialize $ rdfForTask baseURI task)
+      Nothing -> notFound
+router state { path, method: Put, body }
+  | path !@ 0 == "tasks" && not (path !@ 1 == "") = do
+    b <- toString body
+    let gr = parse b
+    case gr of
+      Left parserError -> badRequest $ parseErrorMessage parserError
+      Right gr' -> do
+        let input = taskForRdf (baseURI <> "tasks/" <> path !@ 1) gr'
+        case input of 
+          Nothing -> badRequest "Task could not be extracted from RDF"
+          Just input' -> do
+            _ <- liftEffect $ modify (\s -> { config: s.config, tasks: (Tuple (path !@ 1) (Tuple input' TaskAccepted)) : s.tasks, events: s.events }) state
+            { config } <- liftEffect $ read state
+            delay $ Milliseconds 5000.0
+            let (Tuple output config') = runState (processInput e input') config
+            _ <- case output of
+              Nothing -> liftEffect $ modify (\s -> { config: config', tasks: s.tasks, events: s.events }) state
+              Just output' -> liftEffect $ modify (\s -> { config: config', tasks: s.tasks, events: (Tuple (findNextEventIdx s.events) output') : s.events }) state
+            created
+  where
+    findNextEventIdx :: List (Tuple Int Output) -> Int
+    findNextEventIdx events = case maximum $ map (\(Tuple i _) -> i) events of 
+      Nothing -> 0
+      Just i -> i + 1
 router _ _ = notFound
 
 g :: Graph
@@ -60,5 +94,5 @@ g = fromFoldable [
 
 main :: ServerM
 main = do
-  config <- new initialEFSMConfig
+  config <- new { config: Tuple s0 { pos: 1, closed: true, item: false }, tasks: (Tuple "task2" (Tuple a TaskFailed)) : (Tuple "task1" (Tuple b TaskFailed)) : Nil, events: Nil }
   serve 8080 (router config) $ log "Started server at http://localhost:8080/."
